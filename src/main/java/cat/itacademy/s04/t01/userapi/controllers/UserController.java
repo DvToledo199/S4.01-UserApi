186d9f9a0fbb8e31348e7402e2b96e554d21c300
David Toledo <toledo_1990@hotmail.com>
2026-06-03 21:18:51 +0200

    feat: add user creation endpoint

--- src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
+++ src/main/java/cat/itacademy/s04/t01/userapi/controllers/UserController.java
@@ -2,10 +2,13 @@ package cat.itacademy.s04.t01.userapi.controllers;
 
 import cat.itacademy.s04.t01.userapi.models.User;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 public class UserController {
@@ -16,5 +19,13 @@ public class UserController {
     public List<User> getUsers() {
         return users;
     }
+
+    @PostMapping("/users")
+    public User createUser(@RequestBody User user) {
+        user.setId(UUID.randomUUID());
+        users.add(user);
+        return user;
+    }
+
 
 }
